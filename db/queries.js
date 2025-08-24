faa98a41895bc74c5d9b672789123fa6551131d9
Perry Huang <perryhuang2015@gmail.com>
2025-08-24 11:53:25 -0400

    feat: create queries for CRUD operations on db

--- db/queries.js
+++ db/queries.js
@@ -1,28 +1,79 @@
 const pool = require("./pool");
 
-// Sample queries: change as needed
-
-async function getAllMessages() {
-    const { rows } = await pool.query("SELECT * FROM messages");
-    return rows;
+async function createItem(itemName) {
+    await pool.query("INSERT INTO items (name) values($1)", [itemName]);
 }
 
-async function getMessage(id) {
-    const { rows } = await pool.query("SELECT * FROM messages where id=($1)", [
-        id,
-    ]);
-    return rows;
+async function createPool(poolName) {
+    await pool.query("INSERT INTO pools (name) values($1)", [poolName]);
 }
 
-async function insertMessage(msg, author) {
+async function createItemToPool(itemId, poolId) {
     await pool.query(
-        "INSERT INTO messages (msg, author, date) values(($1), ($2), ($3))",
-        [msg, author, datefns.format(new Date(), "yyyy-MM-dd")],
+        "INSERT INTO item_to_pool (item, pool) values(($1), ($2))",
+        [itemId, poolId],
+    );
+}
+
+async function readAllPools() {
+    const { rows } = await pool.query("SELECT * FROM pools");
+    return rows;
+}
+
+async function readAllItems() {
+    const { rows } = await pool.query("SELECT * FROM items");
+    return rows;
+}
+
+async function readItemsFromPool(poolId) {
+    const { rows } = await pool.query(
+        "SELECT * FROM item_to_pool WHERE pool=($1)",
+        [poolId],
+    );
+    return rows;
+}
+
+async function updateItem(itemId, newName) {
+    await pool.query("UDPATE items SET name=($1) WHERE id=($2)", [
+        newName,
+        itemId,
+    ]);
+}
+
+async function updatePool(poolId, newName) {
+    await pool.query("UDPATE pools SET name=($1) WHERE id=($2)", [
+        newName,
+        poolId,
+    ]);
+}
+
+async function deleteItem(itemId) {
+    await pool.query("DELETE FROM item_to_pool * WHERE item=($1)", [itemId]);
+    await pool.query("DELETE FROM items * where id=($1)", [itemId]);
+}
+
+async function deletePool(poolId) {
+    await pool.query("DELETE FROM item_to_pool * WHERE pool=($1)", [poolId]);
+    await pool.query("DELETE FROM pools * where id=($1)", [poolId]);
+}
+
+async function deleteItemToPool(itemId, poolId) {
+    await pool.query(
+        "DELETE FROM item_to_pool * WHERE pool=($1) AND item=($2)",
+        [poolId, itemId],
     );
 }
 
 module.exports = {
-    insertMessage,
-    getMessage,
-    getAllMessages,
+    createItem,
+    createPool,
+    createItemToPool,
+    readAllItems,
+    readAllPools,
+    readItemsFromPool,
+    updateItem,
+    updatePool,
+    deleteItem,
+    deletePool,
+    deleteItemToPool,
 };
