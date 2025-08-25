57250c847b1bc5e23f72dac3de10cf7e4b85e469
Perry Huang <perryhuang2015@gmail.com>
2025-08-24 21:38:58 -0400

    feat: add splat route

--- routes/readRouter.js
+++ routes/readRouter.js
@@ -3,5 +3,6 @@ const getData = require("../controllers/readController");
 const readRouter = Router();
 
 readRouter.get("/", getData);
+readRouter.get("/{*splat}", getData);
 
 module.exports = readRouter;
